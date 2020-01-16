8f9a05fbbe72eac39c94f3fac26fc926497ef43b
Djordje Nikolic <djordje.nikolic0997@gmail.com>
2020-01-16 20:54:27 +0100

    Extracted models in the library

--- LeaderboardManager/LeaderboardManagerClientLibrary/CryptionService.cs
+++ LeaderboardManager/LeaderboardManagerClientLibrary/CryptionService.cs
@@ -8,16 +8,6 @@ using System.IO;
 
 namespace LeaderboardManagerClientLibrary
 {
-    public class KeyIVPair
-    {
-        public byte[] Key { get; set; }
-        public byte[] IV { get; set; }
-        public KeyIVPair(byte[] key, byte[] iv)
-        {
-            Key = key;
-            IV = iv;
-        }
-    }
     public enum CryptoAlgo { RC2, AES, Rijndael, DES, TripleDES }
     public class CryptionService
     {
--- LeaderboardManager/LeaderboardManagerClientLibrary/Formatter.cs
+++ LeaderboardManager/LeaderboardManagerClientLibrary/Formatter.cs
@@ -7,12 +7,6 @@ using System.Text.RegularExpressions;
 
 namespace LeaderboardManagerClientLibrary
 {
-    public struct UserInfo
-    {
-        public string Name { get; set; }
-        public double Score { get; set; }
-        public string Comment { get; set; }
-    }
 
     public static class Formatter
     {
